ab7163f3ae0e6d1a10381645c3a93949f78c357d
Christo Sojan <christosojan100@gmail.com>
2023-12-27 12:03:54 +0530

    minor fix

--- src/Widget/index.jsx
+++ src/Widget/index.jsx
@@ -48,7 +48,7 @@ Widget.prototype = {
 
 Widget.defaultProps = {
   rasaServerUrl: "http://localhost:5005/webhooks/rest/webhook",
-  userId: "jitesh97",
+  userId: "user1",
   initialPayload: "/greet",
   metadata: {},
   botAvatar:
